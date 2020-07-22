a28deb2f9ddeda5d67c2d5e078da0f7887f8fae9
Zohar Peled <Peled.Zohar@gmail.com>
2020-07-22 17:45:06 +0300

    Add files via upload

---
+++ ResultOf.Tests/ResultBooleanOperatorUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResultOf.Tests
+{
+    class ResultBooleanOperatorUnitTests
+    {
+        private Result _fail, _success;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fail = Result.Fail("fail");
+            _success = Result.Success();
+        }
+
+        [Test]
+        public void FalseOperator_ReturnsFalseOnFailure()
+        {
+            if(_fail)
+            {
+                Assert.Fail("Fail result should have returned false.");
+            }
+            else
+            {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void TrueOperator_ReturnsTrueOnSuccess()
+        {
+            if (_success)
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail("Success result should have returned true."); 
+            }
+        }
+    }
+}
